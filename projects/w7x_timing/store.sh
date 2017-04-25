8db6adc4bfdf804126769866cfa27e58d16f284d
zack-vii <zack-vii@live.com>
2017-04-25 20:53:53 +0200

    Fix: no need for mdsplus on RP

--- projects/w7x_timing/store.sh
+++ projects/w7x_timing/store.sh
@@ -5,8 +5,8 @@ makedest () {
 dest=~/rp/${num}$1
 mkdir -p ${dest}
 }
-makedest /usr/local/mdsplus/tdi/w7x_timing
-cp ${src}/tdi/*.fun $dest ; \
+#makedest /usr/local/mdsplus/tdi/w7x_timing
+#cp ${src}/tdi/*.fun $dest ; \
 makedest /root
 cp ${src}/logic/out/red_pitaya.bit ${src}/src/w7x_timing.ko ${src}/src/w7x_timing_test ${src}/src/W7XTIMING.py $dest && \
 makedest /usr/local/lib
