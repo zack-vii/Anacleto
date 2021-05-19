14015cd8865facaec3cd1cdbd26bd83d1b9ac6f1
Schroeder, Timo (cloud) <timo.schroeder@ipp.mpg.de>
2021-05-19 13:06:59 +0200

    fixed store and update README.md

--- projects/rp_trig2/store.sh
+++ projects/rp_trig2/store.sh
@@ -2,18 +2,24 @@
 src=$(realpath $(dirname $0))
 KVER=${KVER:-4.9.0}
 u512=$src/../usermem
-name=$1
+name=${1:-$(basename $src)}
 makedest () {
 dest=~/rp/${name}$1
 mkdir -p $dest
 }
 makedest /
+rm -rf $dest/*
 cp -rf $src/fs/* $u512/fs/* $dest
 makedest /boot
 cp $u512/devicetree.dtb $src/../../uImage $dest
 makedest /boot/fpga
-cp $src/out/fpga_rptrig.bit $dest
+cp $src/edit/red_pitaya/rp_trig_brd_1.0.bit/rp_trig_brd_1.0.bit $dest/fpga_rptrig.bit
 makedest /lib
-cp $src/out/librptrig.so $dest
+cp $src/linux/librptrig.so $dest
+makedest /bin
+cp $src/linux/rptrig $dest
 makedest /lib/modules/${KVER}-xilinx/
 cp $u512/linux/usermem.ko $dest
+
+makedest
+tar -zcf $dest.tgz -C $dest .
